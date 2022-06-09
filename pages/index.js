d1d3c8cc4b17f5425399232721ba55f1306b8cea
shivam saraswat <shivam@gmail.com>
2022-06-09 10:10:55 +0530

    main page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import classes from "../styles/Home.module.css";
 import Layout from "../components/Layout";
 import { Grid, Typography } from "@mui/material";
-export default function Home() {
+import Image1 from "./../assets/image1.jpg";
+export default function Home(props) {
   return (
-    <div className={styles.container}>
+    <div className={classes.container}>
       <Layout>
+        <h1>hello</h1>
+        <Typography variant="h2">Popular Products</Typography>
         <Grid container spacing={3}>
           <Grid item xs={6}>
             <Typography>content</Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography>content</Typography>
+            <Typography>
+              <Image src={Image1} />
+            </Typography>
           </Grid>
         </Grid>
       </Layout>
