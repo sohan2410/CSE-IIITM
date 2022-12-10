3e4bd8d9734661817eff356bc62446836c7922f3
sohan2410 <bandarysohan24@gmail.com>
2022-12-10 15:41:55 +0530

    route changes

--- components/Profile.js
+++ components/Profile.js
@@ -1,13 +1,20 @@
 import React from "react"
-import { Typography, Card, CardContent, CardMedia, CardActionArea } from "@mui/material"
+import { Typography, Card, CardContent, CardMedia, CardActionArea, Grid } from "@mui/material"
+import MailOutlineIcon from "@mui/icons-material/MailOutline"
+import LanguageIcon from "@mui/icons-material/Language"
+import { Link } from "@mui/material"
+
 const Profile = ({ people }) => {
   return (
-    <Card sx={{ maxWidth: 500 }} style={{ margin: "20px" }}>
+    <Card sx={{ width: 350, height: 550 }} style={{ margin: "20px", boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)" }}>
       <CardActionArea>
-        <CardMedia component="img" height="300" image={people.image} alt={people.name} />
+        <CardMedia component="img" height="300" image={people.image} alt={people.name} style={{ display: "block", magrin: "0 auto" }} />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h5" component="div" textAlign="center">
             {people.name}
+          </Typography>
+          <Typography variant="body1" textAlign="center">
+            {people.designation}
           </Typography>
           <Typography variant="body2" color="text.secondary">
             Area of Interest:{" "}
@@ -21,9 +28,13 @@ const Profile = ({ people }) => {
           <Typography variant="body2" color="text.secondary">
             Address: {people.address}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Contact: <a href={`mailto:${people.email}`}>Email</a> <a href={people.website}>Website</a>
-          </Typography>
+
+          <Link href={people.email} target="_blank">
+            <MailOutlineIcon style={{ color: "black" }} />
+          </Link>
+          <Link href={people.website} target="_blank">
+            <LanguageIcon style={{ color: "black" }} />
+          </Link>
         </CardContent>
       </CardActionArea>
     </Card>
