9b21c773da0f2ab0953cdc0cc06ca4a381673cbb
sohan2410 <bandarysohan24@gmail.com>
2022-12-15 00:16:59 +0530

    added carousel

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import '../styles/globals.css';
-import Layout from '../components/Layout';
+import "../styles/globals.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Layout from "../components/Layout"
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-
       <Component {...pageProps} />
     </Layout>
   )
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import data from "./api/data"
 import createTheme from "@mui/material"
 import Profile from "../components/Profile"
 import news from "./api/news"
+import MyCarousel from "../components/MyCarousel"
 export default function Home(props) {
   const router = useRouter()
 
@@ -14,42 +15,49 @@ export default function Home(props) {
       <Head>
         <title>Home</title>
       </Head>
-      {/* <Layout> */}
-      <Container
-        maxWidth="xl"
-        className={classes.home_box} sx={{}}>
+      <Container maxWidth="xl" className={classes.home_box} sx={{}}>
         <Grid
           container
           spacing={3}
           // padding={'10px 60px'}
           className={classes.container}
         >
-          <Grid
-            item xs={12}
-            md={8}
-            className={classes.intro_box}>
-            <Typography variant="h2"
+          <Grid item xs={12} md={8} className={classes.intro_box}>
+            <Typography
+              variant="h2"
               sx={{
-                fontFamily: 'Roboto Mono',
+                fontFamily: "Roboto Mono",
               }}
-
-              className={classes.intro_heading}>
+              className={classes.intro_heading}
+            >
               Computer Science and Engineering
             </Typography>
-            <div justifyContent={"center"} className={classes.intro_text}
+            <div>
+              <MyCarousel />
+            </div>
+            <div
+              justifyContent={"center"}
+              className={classes.intro_text}
               style={{
-                fontFamily: 'Varela Round',
+                fontFamily: "Varela Round",
                 textAlign: "justify",
                 marginTop: "40px",
                 fontSize: "18px",
-                lineHeight: "1.8"
-              }} dangerouslySetInnerHTML={{ __html: data.introduction }}></div>
+                lineHeight: "1.8",
+              }}
+              dangerouslySetInnerHTML={{ __html: data.introduction }}
+            ></div>
           </Grid>
           <Grid item xs={12} md={4} className={classes.news_box}>
             <div>
-              <Typography variant="h3" className={classes.news_heading} align="center" sx={{
-                fontFamily: 'Roboto Mono',
-              }}>
+              <Typography
+                variant="h3"
+                className={classes.news_heading}
+                align="center"
+                sx={{
+                  fontFamily: "Roboto Mono",
+                }}
+              >
                 News Section
               </Typography>
               <ul>
@@ -60,26 +68,36 @@ export default function Home(props) {
             </div>
           </Grid>
           <Grid item xs={12}>
-
             {/* vision */}
-            <Typography variant="h2" align="center" sx={{
-              fontFamily: 'Roboto Mono',
-            }}>
+            <Typography
+              variant="h2"
+              align="center"
+              sx={{
+                fontFamily: "Roboto Mono",
+              }}
+            >
               Vision
             </Typography>
-            <Typography style={{
-              lineHeight: '1.8',
-              fontSize: '18px',
-              marginTop: '36px',
-              marginBottom: '36px',
-              fontFamily: 'Varela Round',
-            }}>{data.vision}</Typography>
+            <Typography
+              style={{
+                lineHeight: "1.8",
+                fontSize: "18px",
+                marginTop: "36px",
+                marginBottom: "36px",
+                fontFamily: "Varela Round",
+              }}
+            >
+              {data.vision}
+            </Typography>
 
             {/* mission */}
-            <Typography variant="h2" align="center"
+            <Typography
+              variant="h2"
+              align="center"
               sx={{
-                fontFamily: 'Roboto Mono',
-              }}>
+                fontFamily: "Roboto Mono",
+              }}
+            >
               Mission
             </Typography>
             <ul className={classes.mission_text}>
@@ -88,58 +106,64 @@ export default function Home(props) {
                   <li>
                     <Typography
                       sx={{
-                        lineHeight: '1.7',
-                        fontSize: '18px',
-                        fontFamily: 'Varela Round'
-                      }}>{item}</Typography>
+                        lineHeight: "1.7",
+                        fontSize: "18px",
+                        fontFamily: "Varela Round",
+                      }}
+                    >
+                      {item}
+                    </Typography>
                   </li>
                 )
               })}
             </ul>
 
-
             {/* programs offered by the department */}
             <Typography
               variant="h2"
               align="center"
-              marginTop={'40px'} sx={{
-                fontFamily: 'Roboto Mono',
-              }} >
+              marginTop={"40px"}
+              sx={{
+                fontFamily: "Roboto Mono",
+              }}
+            >
               Programs offered by the department
             </Typography>
 
-            <ul className={classes.program_text} >
+            <ul className={classes.program_text}>
               {data.pragrams_offered.map((item) => {
                 return (
                   <li>
                     <Typography
                       sx={{
-                        lineHeight: '1.7',
-                        fontSize: '18px',
-                        fontFamily: 'Varela Round',
-                      }}>{item}</Typography>
+                        lineHeight: "1.7",
+                        fontSize: "18px",
+                        fontFamily: "Varela Round",
+                      }}
+                    >
+                      {item}
+                    </Typography>
                   </li>
                 )
               })}
             </ul>
 
             {/* head of the department */}
-            <Typography
-              variant="h2"
-              align="center" marginTop={'40px'} sx={{fontFamily: 'Roboto Mono',}}>
+            <Typography variant="h2" align="center" marginTop={"40px"} sx={{ fontFamily: "Roboto Mono" }}>
               Head of the department
             </Typography>
             <div
               style={{
                 display: "flex",
-                justifyContent: "center"
-              }}>
+                justifyContent: "center",
+              }}
+            >
               <Profile people={data.head_of_department} />
             </div>
           </Grid>
         </Grid>
       </Container>
       {/* </Layout> */}
-    </div >
+    </div>
   )
 }
